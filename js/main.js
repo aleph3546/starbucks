cc7e1d0bd8f9d658894cccb9e76ceea4facf7dfd
ilwoo Jeong <>
2023-07-24 22:58:40 +0900

    2023.07.25 스크롤 위치 계산 애니메이션

--- js/main.js
+++ js/main.js
@@ -70,6 +70,12 @@ new Swiper('.promotion .swiper', {
     nextEl: '.promotion .swiper-next'
   }
 });
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5
+});
 
 
 const promotionEl = document.querySelector('.promotion');
@@ -109,3 +115,15 @@ function floatingObject(selector, delay, size) {
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
